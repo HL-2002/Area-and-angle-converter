16815a9b4363a1e7847667060a64f6103ad60381
HL-2002 <henryslang8231682@gmail.com>
2024-10-01 15:46:45 -0400

    Format simple angle formulas and finish area ones

--- src/src/main/Angle.java
+++ src/src/main/Angle.java
@@ -16,24 +16,24 @@ public class Angle extends Unit {
     // cambiar las conversiones para que tambien entreguen la formula de conversión
     conversions = Map.of(
       "Arcsecond", Map.of(
-        "Degree", new Conversion(1 / 3600f, "value / 3600"),
-        "Gradian", new Conversion(1 / 3600f * 0.9f, "value / 3600 * 0.9"),
+        "Degree", new Conversion(1 / 3600f, "Divide the value by 3600"),
+        "Gradian", new Conversion(1 / 3240f, "Divide the value by 3240"),
         "Miliradian", new Conversion(1 / 3600f * (float) (Math.PI / 180) * 1000, "value / 3600 * (π / 180) * 1000"),
-        "Minute of arc", new Conversion(1 / 60f, "value / 60"),
+        "Minute of arc", new Conversion(1 / 60f, "Divide the value by 60"),
         "Radian", new Conversion((float) (Math.PI / (180 * 3600)), "value * (π / (180 * 3600))")
       ),
       "Degree", Map.of(
-        "Arcsecond", new Conversion(3600f, "value * 3600"),
-        "Gradian", new Conversion(0.9f, "value * 0.9"),
+        "Arcsecond", new Conversion(3600f, "Multiply the value by 3600"),
+        "Gradian", new Conversion(0.9f, "Multiply the value by 0.9"),
         "Miliradian", new Conversion((float) (Math.PI / 180) * 1000, "value * (π / 180) * 1000"),
-        "Minute of arc", new Conversion(60f, "value * 60"),
+        "Minute of arc", new Conversion(60f, "Multiply the value by 60"),
         "Radian", new Conversion((float) (Math.PI / 180), "value * (π / 180)")
       ),
       "Gradian", Map.of(
-        "Arcsecond", new Conversion(3600f / 0.9f, "value * 3600 / 0.9"),
-        "Degree", new Conversion(1 / 0.9f, "value / 0.9"),
+        "Arcsecond", new Conversion(3240f, "Multiply the value by 3240"),
+        "Degree", new Conversion(1 / 0.9f, "Divide the value by 0.9"),
         "Miliradian", new Conversion((float) (Math.PI / 200) * 1000, "value * (π / 200) * 1000"),
-        "Minute of arc", new Conversion(60f / 0.9f, "value * 60 / 0.9"),
+        "Minute of arc", new Conversion(54f, "Multiply the value by 54"),
         "Radian", new Conversion((float) (Math.PI / 200), "value * (π / 200)")
       ),
       "Miliradian", Map.of(
@@ -44,9 +44,9 @@ public class Angle extends Unit {
         "Radian", new Conversion(0.001f, "value * 0.001")
       ),
       "Minute of arc", Map.of(
-        "Arcsecond", new Conversion(60f, "value * 60"),
-        "Degree", new Conversion(1 / 60f, "value / 60"),
-        "Gradian", new Conversion(1 / 60f * 0.9f, "value / 60 * 0.9"),
+        "Arcsecond", new Conversion(60f, "Multiply the value by 60"),
+        "Degree", new Conversion(1 / 60f, "Divide the value by 60"),
+        "Gradian", new Conversion(1 / 60f * 0.9f, "Divide the value by 54"),
         "Miliradian", new Conversion(1 / 60f * (float) (Math.PI / 180) * 1000, "value / 60 * (π / 180) * 1000"),
         "Radian", new Conversion((float) (Math.PI / (180 * 60)), "value * (π / (180 * 60))")
       ),
@@ -54,7 +54,7 @@ public class Angle extends Unit {
         "Arcsecond", new Conversion((float) (180 * 3600 / Math.PI), "value * (180 * 3600 / π)"),
         "Degree", new Conversion((float) (180 / Math.PI), "value * (180 / π)"),
         "Gradian", new Conversion((float) (200 / Math.PI), "value * (200 / π)"),
-        "Miliradian", new Conversion(1000f, "value * 1000"),
+        "Miliradian", new Conversion(1000f, "Multiply the vallue by 1000"),
         "Minute of arc", new Conversion((float) (180 * 60 / Math.PI), "value * (180 * 60 / π)")
       )
     );
--- src/src/main/Area.java
+++ src/src/main/Area.java
@@ -4,18 +4,96 @@
  */
 package main;
 
+import java.util.Map;
+
 /**
  *
  * @author time
  */
 public class Area extends Unit {
 
-  @Override
+    public Area() {
+        conversions = Map.of(
+            "Square Kilometre", Map.of(
+                "Square metre", new Conversion(1e6f, "Multiply the value by 1e+6"),
+                "Square mile", new Conversion(1/2.59f, "Divide the value by 2.59"),
+                "Square yard", new Conversion(1.196e6f, "Multiply the value by 1.196e+6"),
+                "Square foot", new Conversion(1.076e7f, "Multiply the value by 1.076e+7"),
+                "Square inch", new Conversion(1.55e9f, "Multiply the value by 1.55e+9"),
+                "Hectare", new Conversion(100f, "Multiply the value by 100"),
+                "Acre", new Conversion(247.1f, "Multiply the value by 247.1")
+            ),
+            "Square metre", Map.of(
+                "Square Kilometre", new Conversion(1e-6f, "Divide the value by 1e+6"),
+                "Square mile", new Conversion(1/2.59e6f, "Divide the value by 2.59e+6"),
+                "Square yard", new Conversion(1.196f, "Multiply the value by 1.196"),
+                "Square foot", new Conversion(10.764f, "Multiply the value by 10.764"),
+                "Square inch", new Conversion(1550f, "Multiply the value by 1550"),
+                "Hectare", new Conversion(1e-4f, "Divide the value by 1e4"),
+                "Acre", new Conversion(1/4047f, "Divide the value by 4047")
+            ),
+            "Square mile", Map.of(
+                "Square Kilometre", new Conversion(2.59f, "Multiply the value by 2.59"),
+                "Square metre", new Conversion(2.59e6f, "Multiply the value by 2.59e+6"),
+                "Square yard", new Conversion(3.098e6f, "Multiply the value by 3.098e+6"),
+                "Square foot", new Conversion(2.788e7f, "Multiply the value by 2.788e+7"),
+                "Square inch", new Conversion(4.014e9f, "Multiply the value by 4.014e+9"),
+                "Hectare", new Conversion(259f, "Multiply the value by 259"),
+                "Acre", new Conversion(640f, "Multiply the value by 640")
+            ),
+            "Square yard", Map.of(
+                "Square Kilometre", new Conversion(1/1.196e6f, "Divide the value by 1.196e+6"),
+                "Square metre", new Conversion(1/1.196f, "Divide the value by 1.196"),
+                "Square mile", new Conversion(1/3.098e6f, "Divide the value by 3.098e+6"),
+                "Square foot", new Conversion(9f, "Multiply the value by 9"),
+                "Square inch", new Conversion(1296f, "Multiply the value by 1296"),
+                "Hectare", new Conversion(1/11960, "Divide the value by 11960"),
+                "Acre", new Conversion(1/4840f, "Divide the value by 4840")
+            ),
+            "Square foot", Map.of(
+                "Square Kilometre", new Conversion(1/1.076e7f, "Divide the value by 1.076e+7"),
+                "Square metre", new Conversion(1/10.764f, "Divide the value by 10.764"),
+                "Square mile", new Conversion(1/2.788e7f, "Divide the value by 2.788e+7"),
+                "Square yard", new Conversion(1/9f, "Divide the value by 9"),
+                "Square inch", new Conversion(144f, "Multiply the value by 144"),
+                "Hectare", new Conversion(1/1.076e5f, "Divide the value by 1.076e+5"),
+                "Acre", new Conversion(1/43560f, "Divide the value by 43560")
+            ),
+            "Square inch", Map.of(
+                "Square Kilometre", new Conversion(1/1.55e9f, "Divide the value by 1.55e+9"),
+                "Square metre", new Conversion(1/1550f, "Divide the value by 1550"),
+                "Square mile", new Conversion(1/4.014e9f, "Divide the value by 4.014e+9"),
+                "Square yard", new Conversion(1/1296f, "Divide the value by 1296"),
+                "Square foot", new Conversion(1/144f, "Divide the value by 144"),
+                "Hectare", new Conversion(1/1.55e7f, "Divide the value by 1.55e+7"),
+                "Acre", new Conversion(1/6.273e6f, "Divide the value by 6.273e+6")
+            ),
+            "Hectare", Map.of(
+                "Square Kilometre", new Conversion(0.01f, "Divide the value by 100"),
+                "Square metre", new Conversion(1e4f, "Multiply the value by 1e4"),
+                "Square mile", new Conversion(1/259f, "Divide the value by 259"),
+                "Square yard", new Conversion(11960f, "Multiply the value by 11960"),
+                "Square foot", new Conversion(1.076e5f, "Multiply the value by 107600"),
+                "Square inch", new Conversion(1.55e7f, "Multiply the value by 1.55e+7"),
+                "Acre", new Conversion(2.471f, "Multiply the value by 2.471")
+            ),
+            "Acre", Map.of(
+            "Square Kilometre", new Conversion(1/247.1f, "Divide the value by 247.1"),
+            "Square metre", new Conversion(4047f, "Multiply the value by 4047"),
+            "Square mile", new Conversion(1/640f, "Divide the value by 640"),
+            "Square yard", new Conversion(4840f, "Multiply the value by 4840"),
+            "Square foot", new Conversion(43560f, "Multiply the value by 43560"),
+            "Square inch", new Conversion(6.273e6f, "Multiply the value by 6.273e+6"),
+            "Hectare", new Conversion(1/2.471f, "Divide the value by 2.471")
+            )
+        );
+    }
+    
+    @Override
   protected Result transform(float value, String from, String to) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'transform'");
-
-    // implementar lo mismo que en angle xd
+    Conversion conversion = conversions.get(from).get(to);
+    return new Result(value * conversion.getFactor(), conversion.getFormula());
+  
   }
     
     
